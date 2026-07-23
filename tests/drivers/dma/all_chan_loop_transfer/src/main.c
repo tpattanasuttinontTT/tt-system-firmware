a434074dad3bf0cbc131b4f96adac4f1fda16a4f
Thitiwut Pattanasuttinont <tpattanasuttinont@tenstorrent.com>
2026-07-23 11:22:54 -0400

    Add multichanel loop test
    Signed-off-by: Thitiwut Pattanasuttinont <tpattanasuttinont@tenstorrent.com>

---
+++ tests/drivers/dma/all_chan_loop_transfer/src/main.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2026 Tenstorrent AI ULC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+
+ZTEST_SUITE(dma_m2m_loop, NULL, NULL, NULL, NULL, NULL);
